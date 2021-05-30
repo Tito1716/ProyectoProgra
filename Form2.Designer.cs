3f384dadd142e7a7f71e474924a032e0e8565d18
Tito Arevalo <titotono00@hotmail.es>
2021-05-29 19:12:15 -0600

    2

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -48,8 +48,6 @@ namespace Proyecto
             this.metroTextBox1.Name = "metroTextBox1";
             this.metroTextBox1.Size = new System.Drawing.Size(75, 23);
             this.metroTextBox1.TabIndex = 1;
-            this.metroTextBox1.Text = "metroTextBox1";
-            this.metroTextBox1.Click += new System.EventHandler(this.metroTextBox1_Click);
             // 
             // Form2
             // 
